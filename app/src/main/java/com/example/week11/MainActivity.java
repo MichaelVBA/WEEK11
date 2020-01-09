fdd2797615ddde648fbd01b3fbb68bec69aa6b6e
unknown <admin@IHR80PBF011.continental.edu.pe>
2020-01-08 22:34:32 -0500

    lll

--- app/src/main/java/com/example/week11/MainActivity.java
+++ app/src/main/java/com/example/week11/MainActivity.java
@@ -7,18 +7,29 @@ import com.example.week11.adaptadores.ProductoAdaptador;
 import com.example.week11.helpers.QueueUtils;
 import com.example.week11.modelos.productos;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     QueueUtils.QueueObject queue = null;
     ListView productosList;
     ProductoAdaptador ProductoAdaptador;
+        ArrayList<productos> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         queue = QueueUtils.getInstance(this.getApplicationContext());
+        items = new ArrayList<>();
+        productos.injectContactsFromCloud(queue, items, this);
         productosList = findViewById(R.id.productosList);
-        ProductoAdaptador = new ProductoAdaptador(this, productos.getCollection(), queue.getImageLoader());
+        ProductoAdaptador = new ProductoAdaptador(this, items, queue.getImageLoader());
         productosList.setAdapter(ProductoAdaptador);
+        productos.sendRequestPOST(queue, this);
+    }
+    public void refreshList(){
+        if ( ProductoAdaptador!= null ) {
+            ProductoAdaptador.notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/week11/modelos/productos.java
+++ app/src/main/java/com/example/week11/modelos/productos.java
@@ -1,6 +1,20 @@
 package com.example.week11.modelos;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.week11.MainActivity;
+import com.example.week11.helpers.QueueUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class productos {
     public String Precio ;
@@ -26,5 +40,91 @@ public class productos {
         collection.add(new productos("27", "Leon","https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-7.jpg"));
         collection.add(new productos("28", "Gato","https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-4.jpg"));
         return collection;
+
+    }
+    public static void sendRequestPOST(QueueUtils.QueueObject o, final MainActivity _interface) {
+        String url = "http://rrojasen.alwaysdata.net/purchaseorders.json";
+        url = "http://fipo.equisd.com/api/users/new.json";
+       // url = "http://192.168.58.3:8056/api/users/new.json";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //Do it with this it will work
+                            JSONObject _response = new JSONObject(response);
+                            if (_response.has("objects")) {
+                                JSONObject object_response = null;
+                                try {
+                                    object_response = _response.getJSONObject("objects");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                if ( object_response != null ) {
+                                    try {
+                                        System.out.println(object_response.getInt("id"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("first_name","Michael");
+                params.put("last_name","Barros");
+                params.put("avatar","xxx");
+
+                return params;
+            }
+        };
+        o.addToRequestQueue(stringRequest);
+    }
+    public static void injectContactsFromCloud(final QueueUtils.QueueObject o,
+                                               final ArrayList<productos> productos,
+                                               final MainActivity _interface) {
+        String url = "http://fipo.equisd.com/api/users.json";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        if (response.has("objects")) {
+
+                            try {
+                                JSONArray list = response.getJSONArray("objects");
+                                for (int i=0; i < list.length(); i++) {
+                                    JSONObject o = list.getJSONObject(i);
+                                    productos.add(new productos(o.getString("first_name"),o.getString("last_name"),""));
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            _interface.refreshList(); // Esta función debemos implementarla
+                            // en nuestro activity
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        o.addToRequestQueue(jsonObjectRequest);
     }
 }
+
